98354f97cc64fad7167098de2a5414043556f6d3
D1R0 <38256732+D1R0@users.noreply.github.com>
2018-12-31 19:19:27 +0200

    Update client.py

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import platform
+from discord.utils import get
 #from pathlib import Path #Optional
 
 import os
